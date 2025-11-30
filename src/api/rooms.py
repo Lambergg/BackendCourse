3f85bdef5bbbaf40b74281f8c175e2f062445a41
Ivan Krasnov <agata385@mail.ru>
2025-12-01 00:10:05 +0300

    Any fix

--- src/api/rooms.py
+++ src/api/rooms.py
@@ -8,9 +8,9 @@ router = APIRouter(prefix="/hotels", tags=["Номера"])
 
 
 @router.get("/{hotel_id}/rooms")
-async def get_rooms(hotyel_id: int):
+async def get_rooms(hotel_id: int):
     async with async_session_maker() as session:
-        return await RoomsRepository(session).get_filtered(hotel_id=hotyel_id)
+        return await RoomsRepository(session).get_filtered(hotel_id=hotel_id)
 
 
 @router.get("/{hotel_id}/rooms/{room_id}")
@@ -45,7 +45,7 @@ async def partially_edit_room(
         room_id: int,
         room_data: RoomPatchRequest
 ):
-    _room_data = RoomPatch(hotel_id=hotel_id, **room_data.model_dump())
+    _room_data = RoomPatch(hotel_id=hotel_id, **room_data.model_dump(exclude_unset=True))
     async with async_session_maker() as session:
         await RoomsRepository(session).edit(_room_data, hotel_id=hotel_id, exclude_unset=True, id=room_id)
         await session.commit()
