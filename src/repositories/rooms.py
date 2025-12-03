0a6c9d7de59f5a8593ca0d5d59a8999f551a0591
Ivan Krasnov <agata385@mail.ru>
2025-12-03 17:38:34 +0300

    Add new method in RoomsRepsitory and min change get_rooms endpoint.

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -1,3 +1,9 @@
+from datetime import date
+
+from sqlalchemy import select, func
+
+from src.database import engine
+from src.models.bookings import BookingsOrm
 from src.repositories.base import BaseRepository
 from src.models.rooms import RoomsOrm
 from src.schemas.rooms import Room
@@ -21,3 +27,54 @@ class RoomsRepository(BaseRepository):
     """
     model = RoomsOrm
     schema = Room
+
+    async def get_filtered_by_time(
+            self,
+            hotel_id: int,
+            date_from: date,
+            date_to: date
+    ):
+        """
+        -- CTE общее табличное выражение
+            with rooms_count as (
+                select room_id, count(*) as rooms_reserved from bookings
+                where date_from <= '2025-12-31' and date_to >= '2025-12-29'
+                group by room_id
+            ),
+            rooms_left_result as (
+                select rooms.id as room_id, quantity - coalesce(rooms_reserved, 0) as rooms_left
+                from rooms
+                left join rooms_count on rooms.id = rooms_count.room_id
+            )
+            select * from rooms_left_result
+            where rooms_left > 0
+            ;
+        """
+        rooms_count = (
+            select(BookingsOrm.room_id, func.count("*").label("rooms_reserved"))
+            .select_from(BookingsOrm)
+            .filter(
+                BookingsOrm.date_from <= date_to,
+                BookingsOrm.date_to >= date_from,
+            )
+            .group_by(BookingsOrm.room_id)
+            .cte(name="rooms_count")
+        )
+
+        rooms_left_result = (
+            select(
+                RoomsOrm.id.label("rooms.id"),
+                (RoomsOrm.quantity - func.coalesce(rooms_count.c.rooms_reserved, 0)).label("rooms_left"),
+            )
+            .select_from(RoomsOrm)
+            .outerjoin(rooms_count, RoomsOrm.id == rooms_count.c.room_id)
+            .cte(name="rooms_left_result")
+        )
+
+        query = (
+            select(rooms_left_result)
+            .select_from(rooms_left_result)
+            .filter(rooms_left_result.c.rooms_left > 0)
+        )
+
+        print(query.compile(bind=engine, compile_kwargs={"literal_binds": True}))
