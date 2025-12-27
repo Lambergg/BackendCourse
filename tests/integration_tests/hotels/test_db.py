eabb7aa33125aa8dc6eb2e523b4f5e0570d89620
Ivan Krasnov <agata385@mail.ru>
2025-12-27 17:45:09 +0300

    Add conftest, __init__ models

--- tests/integration_tests/hotels/test_db.py
+++ tests/integration_tests/hotels/test_db.py
@@ -6,4 +6,5 @@ async def test_add_hotel():
     hotel_data = HotelAdd(title="Hotel 3 stars test", location="Сочи")
     async with get_db_manager() as db:
         new_hotel_data = await db.hotels.add(hotel_data)
+        await db.commit()
         print(f'{new_hotel_data=}')
