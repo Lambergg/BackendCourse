b1d48d70f1a3c712e02a8beb56c1c1c71d612f73
Ivan Krasnov <agata385@mail.ru>
2025-12-27 17:18:00 +0300

    Add test_db and asycio pytest

---
+++ tests/integration_tests/hotels/test_db.py
@@ -0,0 +1,9 @@
+from src.api.dependencies import get_db_manager
+from src.schemas.hotels import HotelAdd
+
+
+async def test_add_hotel():
+    hotel_data = HotelAdd(title="Hotel 3 stars test", location="Сочи")
+    async with get_db_manager() as db:
+        new_hotel_data = await db.hotels.add(hotel_data)
+        print(f'{new_hotel_data=}')
