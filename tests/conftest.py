abf403ab78f4bd4be679cb4b3cd3850d0aae50e4
Ivan Krasnov <agata385@mail.ru>
2026-01-04 14:25:40 +0300

    Add register_user test, and mock_hotels, mock_rooms

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
-
+from httpx import AsyncClient, ASGITransport
+from src.main import app
 from src.config import settings
 from src.database import Base, engine_null_pool
 from src.models import *
@@ -15,3 +16,18 @@ async def async_main(check_test_mode):
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+
+@pytest.fixture(scope="session", autouse=True)
+async def register_user():
+    async with AsyncClient(
+        transport=ASGITransport(app=app),
+        base_url="http://test",
+    ) as ac:
+        await ac.post(
+            "/auth/register",
+            json={
+                "email": "test@test.com",
+                "password": "test1234",
+            }
+                                 )
