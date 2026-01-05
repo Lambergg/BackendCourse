68cf923e7fa22e3ec49e5374947121244b2ba06f
Ivan Krasnov <agata385@mail.ru>
2026-01-05 15:27:11 +0300

    Home work for add test booking crud course style

--- tests/integration_tests/bookings/test_db.py
+++ tests/integration_tests/bookings/test_db.py
@@ -13,29 +13,28 @@ async def test_add_booking_crud(db):
         date_to=date(year=2023, month=12, day=20),
         price=100,
     )
-    await db.bookings.add(booking_data)
-    await db.commit()
+    new_booking = await db.bookings.add(booking_data)
 
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert booking
+    assert booking.id == new_booking.id
+    assert booking.room_id == new_booking.room_id
+    assert booking.user_id == new_booking.user_id
 
-async def test_get_booking_crud(db):
-    await db.bookings.get_all()
-
-
-async def test_edit_booking_crud(db):
-    user_id = (await db.users.get_all())[0].id
-    room_id = (await db.rooms.get_all())[0].id
-    booking_data = BookingAdd(
+    update_date = date(year=2023, month=12, day=25)
+    update_booking_data = BookingAdd(
         user_id=user_id,
         room_id=room_id,
         date_from=date(year=2023, month=12, day=10),
-        date_to=date(year=2023, month=12, day=20),
-        price=1000,
+        date_to=update_date,
+        price=100,
     )
-    await db.bookings.edit(booking_data)
-    await db.commit()
+    await db.bookings.edit(update_booking_data, id=new_booking.id)
+    updated_booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert updated_booking
+    assert updated_booking.id == new_booking.id
+    assert updated_booking.date_to == update_date
 
-
-async def test_delete_booking_crud(db):
-    user_id = (await db.users.get_all())[0].id
-    await db.bookings.delete(id=user_id)
-    await db.commit()
+    await db.bookings.delete(id=new_booking.id)
+    booking = await db.bookings.get_one_or_none(id=new_booking.id)
+    assert not booking
