833dbf1c36ba0687418d80d6296c0be110c89102
Ivan Krasnov <agata385@mail.ru>
2026-01-05 15:31:01 +0300

    Add comments

--- tests/integration_tests/bookings/test_db.py
+++ tests/integration_tests/bookings/test_db.py
@@ -2,8 +2,9 @@ from datetime import date
 
 from src.schemas.bookings import BookingAdd
 
-
+#Тесты для CRUD операций с бронями
 async def test_add_booking_crud(db):
+    #Добавляем бронь
     user_id = (await db.users.get_all())[0].id
     room_id = (await db.rooms.get_all())[0].id
     booking_data = BookingAdd(
@@ -15,12 +16,14 @@ async def test_add_booking_crud(db):
     )
     new_booking = await db.bookings.add(booking_data)
 
+    #Получаем бронь
     booking = await db.bookings.get_one_or_none(id=new_booking.id)
     assert booking
     assert booking.id == new_booking.id
     assert booking.room_id == new_booking.room_id
     assert booking.user_id == new_booking.user_id
 
+    #Обновляем бронь
     update_date = date(year=2023, month=12, day=25)
     update_booking_data = BookingAdd(
         user_id=user_id,
@@ -35,6 +38,7 @@ async def test_add_booking_crud(db):
     assert updated_booking.id == new_booking.id
     assert updated_booking.date_to == update_date
 
+    #Удаляем бронь
     await db.bookings.delete(id=new_booking.id)
     booking = await db.bookings.get_one_or_none(id=new_booking.id)
     assert not booking
