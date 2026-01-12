fbf1455c6faa192b17df297736895ddc04dd0774
Ivan Krasnov <agata385@mail.ru>
2026-01-12 15:39:28 +0300

    Add parametrize test_add_booking and new [arametrize test_add_and_get_my_bookings

--- tests/integration_tests/bookings/test_api.py
+++ tests/integration_tests/bookings/test_api.py
@@ -1,15 +1,67 @@
-async def test_add_booking(db, authenticated_ac):
-    room_id = (await db.rooms.get_all())[0].id
+import pytest
+
+from tests.conftest import get_db_null_pool
+
+
+@pytest.mark.parametrize("room_id, date_from, date_to, status_code", [
+    (1, "2024-01-01", "2024-01-07", 200),
+    (1, "2024-01-01", "2024-01-07", 200),
+    (1, "2024-01-01", "2024-01-07", 200),
+    (1, "2024-01-01", "2024-01-07", 200),
+    (1, "2024-01-01", "2024-01-07", 200),
+    (1, "2024-01-01", "2024-01-07", 500),
+])
+async def test_add_booking(
+        room_id, date_from, date_to, status_code,
+        db, authenticated_ac
+):
+    #room_id = (await db.rooms.get_all())[0].id
     response = await authenticated_ac.post(
         "/bookings",
         json={
             "room_id": room_id,
-            "date_from": "2024-01-01",
-            "date_to": "2024-01-07",
+            "date_from": date_from,
+            "date_to": date_to,
+        }
+    )
+    assert response.status_code == status_code
+    if status_code == 200:
+        res = response.json()
+        assert isinstance(res, dict)
+        assert res["Status"] == "Ok"
+        assert "data" in res
+
+
+@pytest.fixture(scope="module")
+async def delete_all_bookings():
+    async for _db in get_db_null_pool():
+        await _db.bookings.delete()
+        await _db.commit()
+
+
+@pytest.mark.parametrize("room_id, date_from, date_to, booked_rooms", [
+    (1, "2024-01-01", "2024-01-07", 1),
+    (1, "2024-01-02", "2024-01-08", 2),
+    (1, "2024-01-03", "2024-01-09", 3),
+])
+async def test_add_and_get_my_bookings(
+        room_id,
+        date_from,
+        date_to,
+        booked_rooms,
+        delete_all_bookings,
+        authenticated_ac,
+):
+    response = await authenticated_ac.post(
+        "/bookings",
+        json={
+            "room_id": room_id,
+            "date_from": date_from,
+            "date_to": date_to,
         }
     )
     assert response.status_code == 200
-    res = response.json()
-    assert isinstance(res, dict)
-    assert res["Status"] == "Ok"
-    assert "data" in res
+
+    response_my_bookings = await authenticated_ac.get("/bookings/me")
+    assert response_my_bookings.status_code == 200
+    assert len(response_my_bookings.json()) == booked_rooms
