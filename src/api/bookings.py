95cf7939905436406b8717cbfa47c23e87250382
Ivan Krasnov <agata385@mail.ru>
2026-01-20 16:40:38 +0300

    Add try/except into the code

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 from fastapi_cache.decorator import cache
 
 from src.api.dependencies import DBDep, UserIdDep
-from src.exceptions import ObjectNotFoundException
+from src.exceptions import ObjectNotFoundException, AllRoomsAreBookedException
 from src.schemas.bookings import BookingAddRequest, BookingAdd
 from src.schemas.hotels import Hotel
 from src.schemas.rooms import Room
@@ -37,7 +37,10 @@ async def add_booking(
     hotel: Hotel = await db.hotels.get_one(id=room.hotel_id)
     room_price: int = room.price
     _booking_data = BookingAdd(user_id=user_id, price=room_price, **booking_data.model_dump())
-    booking = await db.bookings.add_booking(_booking_data, hotel_id=hotel.id)
+    try:
+        booking = await db.bookings.add_booking(_booking_data, hotel_id=hotel.id)
+    except AllRoomsAreBookedException as ex:
+        raise HTTPException(status_code=409, detail=ex.detail)
     await db.commit()
 
     return {"Status": "Ok", "data": booking}
