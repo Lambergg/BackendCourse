aac2bdea71ceba9f9a2b9e8b9e94afcbd860c9b0
Ivan Krasnov <agata385@mail.ru>
2026-02-10 18:12:31 +0300

    fix

--- src/main.py
+++ src/main.py
@@ -106,7 +106,7 @@ if __name__ == "__main__":
     - port: 8080 — порт сервера.
     - reload: True — автоматическая перезагрузка при изменении кода (для разработки).
     """
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
 
 """
