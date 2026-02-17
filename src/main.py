54dbb3e0e1a50949111c4c20aae885445b39cf1c
Ivan Krasnov <agata385@mail.ru>
2026-02-17 13:19:25 +0300

    Add CORS and html

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ import logging
 import sys
 from pathlib import Path
 
-from fastapi import FastAPI, Request, status, HTTPException
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.exceptions import RequestValidationError
 from fastapi.openapi.docs import get_swagger_ui_html
 import uvicorn
@@ -66,6 +67,9 @@ async def custom_swagger_ui_html():
         swagger_js_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js",
         swagger_css_url="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css",
     )
+
+
+app.add_middleware(CORSMiddleware, allow_origins=["http://localhost:63342/"])
 
 
 @app.exception_handler(RequestValidationError)
